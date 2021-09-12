5c67701a38f9f824f368fa5fedac023a068215ae
Kristjan Broder Lund <kristjan.lund@marel.com>
2021-09-12 20:51:56 +0000

    WIP: fix(gpmf): use tsmp to sync gyro
    Not perfectly in sync. Maybe 8 to 10 ms off.
    
    TODO: fix last stream. devides the last stream samples over the delta
    between second last and last streams (1 second). Example, spreads the
    17 samples from the last stream over 1 second. Should maybe assume
    200hz or based by previous streams.
    
    fixes #51

--- GPMF_gyro.py
+++ GPMF_gyro.py
@@ -42,35 +42,82 @@ class Extractor:
         num_frames = video.get(cv2.CAP_PROP_FRAME_COUNT)
         self.fps = video.get(cv2.CAP_PROP_FPS)
         self.video_length =  num_frames / self.fps
-        #print("Video length: {} s, framerate: {} FPS".format(self.video_length,self.fps))
+        print("Video length: {} s, framerate: {} FPS".format(self.video_length,self.fps))
 
         self.size = int(video.get(cv2.CAP_PROP_FRAME_WIDTH)), int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
         video.release()
 
     def parse_gyro(self):
-        for frame in self.parsed:
-            for stream in frame["DEVC"]["STRM"]:
+        stream_start_ns = []
+        first_frame_offset = -1
+        shutter_speed = 1/120 # default. TODO: read from gpmf
+        
+        # get gyro and timestamps from gpmf
+        # timestamps (TSMP) are nanoseconds time when the stream starts
+        for fi, frame in enumerate(self.parsed):
+            for si, stream in enumerate(frame["DEVC"]["STRM"]):
+                # print(stream["STNM"], int.from_bytes(stream['STMP'], byteorder='big')) #
                 if "GYRO" in stream:
-                    #print(stream["STNM"]) # print stream name
                     self.gyro += stream["GYRO"]
+                    stream_time_ns = int.from_bytes(stream['STMP'], byteorder='big')
+                    stream_start_ns += [(stream_time_ns, stream['TSMP'])]
+                    print('stream start: {} micro seconds = {}ms - samples: {}'.format(stream_time_ns, stream_time_ns / 1000, len(stream["GYRO"])))
+                    # if si != 0:
+                        # print('STMP: {}'.format(int.from_bytes(stream['STMP'], byteorder='big')))
+                        # print('STMP: {}'.format(struct.unpack_from('<i', stream['STMP'], 4)[0]))
                     
                     # Calibration scale shouldn't change
                     self.gyro_scal = stream["SCAL"]
-                    #print(self.gyro_scal)
+                    #print(self.gyro_scal) print stream name
+                if first_frame_offset == -1 and "SHUT" in stream:
+                    first_frame_offset = int.from_bytes(stream['STMP'], byteorder='big')
+                    # print(stream['SHUT'])
+
+                if fi == 0 and "SHUT" in stream:
+                    print(f"First SHUT in stream {si} at {int.from_bytes(stream['STMP'], byteorder='big')}")
+                if fi == 0 and "CORI" in stream:
+                    print(f"First CORI in stream {si} at {int.from_bytes(stream['STMP'], byteorder='big')}")
         
         
+        print("first frame: {} - first gyro: {} = {}".format(first_frame_offset, stream_start_ns[0][0], stream_start_ns[0][0] - first_frame_offset))
+
         # Convert to angular vel. vector in rad/s
         omega = np.array(self.gyro) / self.gyro_scal
         self.num_gyro_samples = omega.shape[0]
 
+        # print('GPMF: {}'.format(omega[0]))
+        # print('frame: {}'.format(self.parsed[0]['DEVC']['STRM'][1]))
+        # print('stream_start_ns: {}, length: {}'.format(stream_start_ns, len(stream_start_ns)))
+        # print(self.num_gyro_samples)
 
         self.gyro_rate = self.num_gyro_samples / self.video_length 
-        #print("Gyro rate: {} Hz, should be close to 200 or 400 Hz".format(self.gyro_rate))
+        print("Gyro rate: {} Hz, should be close to 200 or 400 Hz".format(self.gyro_rate))
 
+        timestamps = []
+        for i, tuple in enumerate(stream_start_ns):
+            prev_tuple = stream_start_ns[i-1] if i > 0 else (0,0)
+            next_tuple = stream_start_ns[i+1] if i+1 < len(stream_start_ns) else stream_start_ns[i-1]
+            deltaNs = abs(next_tuple[0] - tuple[0])
+            deltaSamples = abs(tuple[1] - prev_tuple[1])
+            ns_per_sample = deltaNs / deltaSamples
+            # print(i, deltaNs, deltaSamples, ns_per_sample, tuple)
+            s_per_sample = ns_per_sample / 1000000
+            print(f"next timestamps: \t {1/s_per_sample}hz \t * {deltaSamples} = {deltaNs} \t (from {tuple[0]})")
+            timestamps.append(np.arange(deltaSamples) * s_per_sample + ((tuple[0] - first_frame_offset)/1000000))
+
+
+        # timestamps = []
+        # for i, tuple in enumerate(stream_start_ns):
+
+        # print(timestamps[0], timestamps[1])
+
+
+        # timestamps = np.concatinate([np.arange()])
 
         self.parsed_gyro = np.zeros((self.num_gyro_samples, 4))
-        self.parsed_gyro[:,0] = np.arange(self.num_gyro_samples) * 1/self.gyro_rate
+        # self.parsed_gyro[:,0] = np.arange(self.num_gyro_samples) * 1/self.gyro_rate
+        self.parsed_gyro[:,0] = np.concatenate(timestamps)
 
         # Data order for gopro gyro is (z,x,y)
         self.parsed_gyro[:,3] = omega[:,0] # z
